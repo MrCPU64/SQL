09696762bed09b0fe52f18ae4560017faba190e8
MrCPU64 <76919938+MrCPU64@users.noreply.github.com>
2022-01-25 18:34:14 +0300

    Delete sqlodev1.sql

--- sqlodev1.sql
+++
@@ -1,6 +0,0 @@
---SELECT title, description FROM film;
---SELECT * FROM film WHERE film.length > 60 AND film.length < 75;
---SELECT * FROM film 
---WHERE film.rental_rate = 0.99 AND (film.replacement_cost = 12.99 OR film.replacement_cost = 28.99);
---SELECT last_name FROM customer WHERE customer.first_name = 'Mary';
-SELECT * FROM film WHERE film.length <= 50 AND (film.rental_rate != 2.99 And film.rental_rate != 4.99);
